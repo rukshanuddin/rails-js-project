c147143010b41564d6476c8fdb61363758d8923f
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-04-26 23:25:00 -0400

    custom create and strong params

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -15,10 +15,10 @@ class CommentsController < ApplicationController
 
   # POST /comments
   def create
-    @comment = Comment.new(comment_params)
+    @comment = Comment.create(comment_params)
 
-    if @comment.save
-      render json: @comment, status: :created, location: @comment
+    if @comment.valid?
+      render json: @comment
     else
       render json: @comment.errors, status: :unprocessable_entity
     end
@@ -39,13 +39,14 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
+  
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
     # Only allow a trusted parameter "white list" through.
-    def comment_params
-      params.require(:comment).permit(:user_id)
-    end
+  def comment_params
+    params.permit(:comment, :user_id, :content, :t_user_profile_image_url, :t_user_screen_name, :t_text, :t_user_profile_banner_url, :t_user_name)
+  end
 end
