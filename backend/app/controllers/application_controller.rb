f49dabb7a7491c0c0fb13f3e1cf25406d8c279ac
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-04-27 06:19:42 -0400

    delete whitespace

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::API
-    CLIENT = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['KEY']
-      config.consumer_secret     = ENV['SECRET']
-      config.access_token        = ENV['ACCESS_TOKEN']
-      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
-    end
+  CLIENT = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV['KEY']
+    config.consumer_secret     = ENV['SECRET']
+    config.access_token        = ENV['ACCESS_TOKEN']
+    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+  end
 end
