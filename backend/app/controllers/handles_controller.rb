4c7676b1290b621402467c2f4a2f1956850b5ec7
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-04-26 23:25:40 -0400

    removed pry and association to tweets

--- backend/app/controllers/handles_controller.rb
+++ backend/app/controllers/handles_controller.rb
@@ -1,7 +1,5 @@
-require 'pry'
 class HandlesController < ApplicationController
   before_action :set_handle, only: [:show, :update, :destroy]
-  
 
   # GET /handles
   def index
@@ -11,19 +9,12 @@ class HandlesController < ApplicationController
 
   # GET /handles/1
   def show
-    
     redirect_to handle_tweets_path(@handle)
   end
 
   # POST /handles
   def create
     @handle = Handle.find_or_create_by(handle_params)
-
-    if @handle.valid?
-      render json: @handle, status: :created, location: @handle
-    else
-      render json: @handle.errors, status: :unprocessable_entity
-    end
   end
 
   # PATCH/PUT /handles/1
@@ -41,13 +32,14 @@ class HandlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_handle
-      @handle = Handle.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def handle_params
-      params.require(:handle).permit(:name, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_handle
+    @handle = Handle.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def handle_params
+    params.require(:handle).permit(:name, :user_id)
+  end
 end
