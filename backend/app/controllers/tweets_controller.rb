aa8a6dc68f9ea052b95f6e17e0f2250df9c1cc69
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-04-30 00:16:34 -0400

    deleted unecessary controller actions

--- backend/app/controllers/tweets_controller.rb
+++ backend/app/controllers/tweets_controller.rb
@@ -1,52 +1,7 @@
 class TweetsController < ApplicationController
-  before_action :set_tweet, only: %i[show update destroy]
-
-  # GET /tweets
   def index
     @handle = Handle.find(params[:handle_id])
     @tweets = CLIENT.user_timeline(@handle.name, tweet_mode: 'extended')
     render json: @tweets
-  end
-
-  # GET /tweets/1
-  def show
-    render json: @tweet
-  end
-
-  # POST /tweets
-  def create
-    @tweet = Tweet.new(tweet_params)
-
-    if @tweet.save
-      render json: @tweet, status: :created, location: @tweet
-    else
-      render json: @tweet.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /tweets/1
-  def update
-    if @tweet.update(tweet_params)
-      render json: @tweet
-    else
-      render json: @tweet.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /tweets/1
-  def destroy
-    @tweet.destroy
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_tweet
-    @tweet = Tweet.find(params[:id])
-  end
-
-  # Only allow a trusted parameter "white list" through.
-  def tweet_params
-    params.require(:tweet).permit(:handle_id)
   end
 end
