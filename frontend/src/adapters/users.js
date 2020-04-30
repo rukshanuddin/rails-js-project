8beb9c6e1c3ec565550a07f581b83c5a0989ca16
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-04-30 15:36:09 -0400

    delete unecessary  files from frontend

--- frontend/src/adapters/users.js
+++
@@ -1,9 +0,0 @@
-class UsersAdapter {
-    constructor() {
-        this.baseURL = "http://localhost:3000/users"
-    }
-
-    getUsers() {
-        return fetch("http://localhost:3000/users").then(response => response.json())
-    }
-}
